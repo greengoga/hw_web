124e55942841be14e1e419c93ceb132012effcc8
O.Gogol <o.gogol91@gmail.com>
2025-01-18 15:43:01 +0000

    task #2 fixed

--- src/test/java/ru/netology/appOrderTest.java
+++ src/test/java/ru/netology/appOrderTest.java
@@ -12,10 +12,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
+import java.util.function.BooleanSupplier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class appOrderTest {
+class AppOrderTest {
     private WebDriver driver;
 
     @BeforeAll
@@ -46,8 +47,8 @@ class appOrderTest {
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+99999999999");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
 
     @Test
@@ -57,7 +58,7 @@ class appOrderTest {
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+99999999999");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.className("button")).click();
-        String text = driver.findElement(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[3]")).getText();
+        String text = form.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
 
@@ -68,7 +69,7 @@ class appOrderTest {
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+првоп95ваор5");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.className("button")).click();
-        String text = driver.findElement(By.xpath("//*[@id=\"root\"]/div/form/div[2]/span/span/span[3]")).getText();
+        String text = form.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
     }
 
@@ -78,7 +79,27 @@ class appOrderTest {
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Федор Двинятин");
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+99999999");
         form.findElement(By.className("button")).click();
-        WebElement formAfterClick = driver.findElement(By.cssSelector("form"));
-        assertNotNull(formAfterClick, "Form should not be submitted when checkbox is unchecked.");
+        WebElement check = driver.findElement(By.cssSelector(".input_invalid[data-test-id=agreement]"));
+        assertTrue(check.isDisplayed());
+    }
+
+    @Test
+    void shouldFailIfNameUnfilled() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+99999999999");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.className("button")).click();
+        String text = form.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void shouldFailIfPhoneUnfilled() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Йцукен Фыва");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.className("button")).click();
+        String text = form.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
     }
 }
